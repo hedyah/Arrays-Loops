5ce79b4f55d42106f5ae400a0fa64a1a2f0e9f63
Hedwige Uhllanda <hedwigeuhllanda@Hedwiges-MacBook-Air.local>
2022-03-17 22:23:16 -0600

    I added the arrays for the tweets and the loop

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//this is the array of tweets that i found on google//
+
+var tweets = ["I need less stress and I need more fries", 
+"kinda rude when I spend money and it actually leaves my bank account but ok", 
+"Does anyone else get the random urges at midnight to get their life together lol", 
+"I was having problems with my work computer so I called my IT guy and he was like, 'You really need to stop calling me when Im at school, Mom.", 
+"I don't think it's a coincidence that diet has the word die in it.", 
+"My dog sighs a lot for somebody who doesn't contribute to this house or know what a government is",
+"It's called the weekend yet weeks keep happening lol", 
+"I've been alive 20 years and still haven't found the right thing to say when someone knocks on the door of the public bathroom you're in.",
+"You ever accidentally make eye contact with someone 10 + times?", 
+"When you're both typing so you erase the message to let them speak first, but they do the same thing lol." ]
+
+//this is the loop//
+
+for (var counter = 0; counter < tweets.length; counter++){
+    console.log(tweets[counter]);
+}
